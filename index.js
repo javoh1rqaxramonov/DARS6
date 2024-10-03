2e15d1a9638418bee8b5a1dc398899567235e3c1
javoh1rqaxramonov <ubotirov22@.com>
2024-10-03 12:45:50 +0500

    finished

---
+++ index.js
@@ -0,0 +1,464 @@
+// 1-masala 
+// for (let i = 1; i <=50; i++) {
+//     console.log(i);
+// }
+
+// 2-masala 
+// for (let i = 1; i<=20; i++) {
+// console.log(i);
+    
+// }
+
+// 3-masala 
+// let son = +prompt("sonni kiriting");
+// for (let i = 1; i <= son; i++) {
+//     console.log(i**2);  
+// }
+
+// 4-masala 
+// for (let i = 0; i <=10; i++) {
+//     if (i%2==0) {
+//         console.log(i);
+        
+//     }
+    
+// }
+
+//  5-masala 
+// for (let i = 1; i <=15; i++) {
+//     if (i%2==1) {
+//         console.log(i);
+        
+//     }
+    
+// }
+
+// 6-masala 
+// let son1 = +prompt("1-sonni kiriting");
+// let son2 = +prompt("2-sonni kiriting");
+// let son3 = +prompt("3-sonni kiriting");
+// let son4 = +prompt("4-sonni kiriting");
+// let son5 = +prompt("5-sonni kiriting");
+
+// if (typeof son1 == "number") {
+//     console.log(son1);
+//     console.log(son2);
+//     console.log(son3);
+//     console.log(son4);
+//     console.log(son5); 
+// }
+
+// 7-masala 
+// for (let i = 15; i >=0; i--) {
+//     console.log(i);
+    
+    
+// }
+
+// 8-masala 
+// let son = +prompt('sonni kiriting');
+// for (let i = 1; i <=son; i++) {
+//     if (son%i===0) {
+//         console.log(i);
+        
+//     }
+    
+// }
+
+// 9-masala 
+// let counter = 0 ;
+// for (let i = 0; i <=10; i++) {
+//     counter+=i;
+    
+// }
+// console.log(counter);
+
+// 10-masala 
+// for (let i = 1; i <=50; i++) {
+//     if (i%5==0) {
+//         console.log(i);
+        
+//     }    
+// }
+
+// 11-masala 
+// for (let i = 100; i>=1; i--) {
+//     console.log(i);
+    
+// }
+
+// 12-masala 
+// let sum = 0;
+// for (let i = 1; i <=100; i++) {
+//     sum+=i;
+    
+// }
+// console.log(sum);
+
+// 13-masala 
+// let son1 = +prompt("birinchi sonni kiriting");
+// let son2 = +prompt("oxirgi sonni kiriting");
+// for (let i = son1; i <=son2; i++) {
+//     console.log(i);
+    
+    
+// }
+
+// 14-masala 
+// let son = +prompt("sonni kiriting");
+// for (let i = 0; i<=son; i++) {
+//     if (son%i==0) {
+//         console.log(i);
+        
+//     }
+    
+// }
+
+// 15-masala 
+// for (let i = 1; i <=20; i++) {
+//     console.log(i**2);
+    
+// }
+
+// 16-masala 
+// for (let i = 1; i <=100; i++) {
+//     if (i%7==0) {
+//         console.log(i);
+        
+//     }    
+// }
+
+// 17-masala 
+// 18-masala 
+// let son = +prompt("sonni kiriting");
+// let sum = 0;
+// for (let i = 1; i <=son; i++) {
+//     if (son%i==0) {
+//         sum+=i;
+//     }    
+// }
+// console.log(sum);
+
+// 19-masala 
+// let sum = 0;
+// for (let i = 1; i <=50; i++) {
+//     if (i%2==1) {
+//         sum+=i;
+//     }    
+// }
+// console.log(sum);
+
+// 20-masala 
+// let son1 = +prompt("1-sonni kiritng");
+// let son2 = +prompt("2-sonni kiriting");
+// for (let i = son1; i <=son2; i++) {
+//     if (i%2==0) {
+//         console.log(i);
+        
+//     }
+    
+// }
+
+// 21-masala 
+// let sum = 0;
+// for (let i = 1; i <=100; i++) {
+//     if (i%2==0) {
+//         sum+=i;
+//     }    
+// }
+// console.log(sum);
+
+// 22-masala 
+// let son1 = +prompt("sonni kiriting");
+// let p =1;
+// for (let i = 1; i <=son1; i++) {
+//     p*=i
+    
+// }
+// console.log(p);
+
+// 23-masala 
+// for (let i = 1; i <=50; i++) {
+//     if (i%3 !== 0 && i%5 !== 0) {
+//         console.log(i);
+        
+//     }    
+// }
+
+// 24-masala 
+// for (let i = 1; i <=50; i++) {
+//     if (i%4==0 && i%6==0) {
+//         console.log(i);
+        
+//     }    
+// }
+
+// 25-masala
+// let son1 = +prompt("1-sonni kiritng");
+// let son2 = +prompt("2-sonni kiriting");
+// let sum = 0;
+// for (let i = son1; i <=son2; i++) {
+//     if (i%2==1) {
+//         sum+=i;
+//         console.log(i);
+        
+//     }
+// }
+// console.log(sum);
+
+// qoshimcha jazo masalalar
+// qoshimcha jazo masalalar
+// qoshimcha jazo masalalar
+// qoshimcha jazo masalalar
+
+
+// 1-masala
+// let a = 4;
+// if (a>0) {
+//     console.log("Musbat");
+    
+// } else {
+//     console.log("Manfiy");
+    
+// }
+
+// 2-masala
+// let a = 3 ;
+// let b = 6 ;
+// if (a%2==0 && b%2==0) {
+//     console.log("juft son");
+    
+// } else{
+//     console.log("juft emas");
+    
+// }
+
+// 3-masala 
+// let a =3;
+// let b = 4;
+// let c = 3;
+// if (a==b && a==c && b==c) {
+//     console.log("barcha sonlar teng");
+    
+// } else {
+//     console.log("sonlar teng emas");
+    
+// }
+
+// 4-masala
+// let a = 5;
+// if (a>0 && a<100) {
+//     console.log("ortacha son");
+    
+// } else {
+//     console.log("chetdan tashqari");
+    
+// }
+
+// 5-masala
+// let a = 5;
+// if (a<0) {
+//     console.log("muzlash");
+    
+// } else{
+//     console.log("oddiy");
+    
+// }
+
+// 6-masala
+// let a =4;
+// if (a%2==1) {
+//     console.log("toq");
+    
+// } else {
+//     console.log("juft");
+    
+// }
+
+// 7-masala
+// let a = 35;
+// if (a%5==0) {
+//     console.log("bolinadi");
+    
+// } else {
+//     console.log("bolinmaydi");
+    
+// }
+
+// 8-masala 
+// let a =45;
+// if (a>=18) {
+//     console.log("voyaga yetgan");
+    
+// } else {
+//     console.log("voyaga yetmagan");
+    
+// }
+
+// 9-masala 
+// let a = 2024;
+// if (a%4==0) {
+//     console.log("kabisa yili");
+    
+// } else {
+//     console.log("oddiy yil");
+    
+// }
+
+// 10-masala
+// let a = 4;
+// let b = -9;
+// if (a>0 || a<0 && b>0 || b<0) {
+//     console.log("musbat va manfiy");
+    
+// } else {
+//     console.log("har xil emas");
+    
+// }
+
+// 11-masala 
+// let a = 190;
+// let b = 99;
+// if (a>=200 && b>=100) {
+//     console.log("baland va ogir");
+    
+// } else {
+//     console.log("oddiy");
+    
+// }
+
+// 12-masala 
+// let a =7;
+// if (a>=1 && a<=5) {
+//     console.log("ish kuni");
+    
+// } else if(a>=6 && a<=7) {
+//     console.log("dam olish kuni");
+    
+// }
+// if (a>7) {
+//     console.log("bunday raqamdagi hafta kuni mavjut emas");
+    
+// }
+
+// 13-masala 
+// let a = 13 ; 
+// if (a==12) {
+//     console.log("tush payti");
+    
+// } else {
+//     console.log("tush emas");
+    
+// }
+
+// 14-masala
+// let a = 4;
+// if (a>=5) {
+//     console.log("otgan");
+    
+// } else {
+//     console.log("otmagan");
+    
+// }
+
+// 15-masala 
+// let a = 12 ;
+// let b =89;
+// let sum =0 ;
+// sum=a+b;
+// if (sum>=100) {
+//     console.log("katta yigindi");
+    
+// } else {
+//     console.log("kichik yigindi");
+    
+// }
+
+// 16-masala 
+// let a = 6;
+// if (a>0 && a%2==0) {
+//     console.log("musbat juft");
+    
+// } else {
+//     console.log("musbat juft emas");
+    
+// }
+
+// 17-masala 
+// let a = 45;
+// if (a>=18 && a<=65) {
+//     console.log("ishga yaroqli");
+    
+// } else if( a<=17 && a>65){
+//     console.log("ishga yaroqsiz");
+    
+// }
+
+// 18-masala 
+// let a = "may";
+// if (a=="iyun" || a=="iyul" || a=="avgust") {
+//     console.log("issiq");
+    
+// } else {
+//     console.log("sovuq");
+    
+// }
+
+// 19-masala 
+// let a = 4;
+// if (a>=6 && a<=12) {
+//     console.log("ertalab");
+    
+// } else {
+//     console.log("kun emas");
+    
+// }
+
+// 20-masala 
+// let a = "javohir";
+// let b = 12;
+// if (a=="javohir" && b==16) {
+//     console.log("malumot togri");
+    
+// } else {
+//     console.log("malumot notogri");
+    
+// }
+
+// 21-masala 
+// for (let i = 1; i <=10; i++) {
+//     console.log(i);
+    
+    
+// }
+
+// 22-masala 
+// for (let i = 10; i >=1; i--) {
+//     console.log(i);
+    
+    
+// }
+
+// 23-masala 
+// for (let i = 1; i <=20; i++) {
+//     if (i%2==0) {
+//         console.log(i);
+        
+//     }    
+// }
+
+// 24-masala 
+// for (let i = 1; i <=20; i++) {
+//     if (i%2==1) {
+//         console.log(i);
+        
+//     }    
+// }
+
+// 25-masala 
+// for (let i = 1; i <=100; i++) {
+//     if (i%5==0) {
+//         console.log(i);
+        
+//     }    
+// }
